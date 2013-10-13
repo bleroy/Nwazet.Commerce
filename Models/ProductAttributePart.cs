94ca81f3eea14ba9a640eca02c654a9043387916
Bertrand Le Roy <bertrandleroy@gmail.com>
2013-10-12 18:52:48 -0700

    #10: attribute values are now trimmed for validation, eleminating a potential when adding to cart.

--- Models/ProductAttributePart.cs
+++ Models/ProductAttributePart.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Orchard.ContentManagement;
 using Orchard.Environment.Extensions;
 
 namespace Nwazet.Commerce.Models {
     [OrchardFeature("Nwazet.Attributes")]
-    public class ProductAttributePart: ContentPart<ProductAttributePartRecord> {
+    public class ProductAttributePart : ContentPart<ProductAttributePartRecord> {
         public IEnumerable<string> AttributeValues {
             get {
                 return Record.AttributeValues == null
                     ? new string[0]
-                    : Record.AttributeValues.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                    : Record.AttributeValues
+                        .Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(v => v.Trim());
             }
             set {
                 Record.AttributeValues = value == null
-                                             ? null
-                                             : String.Join("\r\n", value);
+                    ? null
+                    : String.Join("\r\n", value.Select(v => v.Trim()));
             }
         }
     }
