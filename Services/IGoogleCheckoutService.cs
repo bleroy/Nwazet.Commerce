94c5b57dc7ecb49a3138cf72e8c617011ef84ac8
Bertrand Le Roy <bertrandleroy@gmail.com>
2013-11-24 21:10:41 -0800

    Removed deprecated Google Checkout feature (Google retired the service)

--- Services/IGoogleCheckoutService.cs
+++
@@ -1,7 +0,0 @@
-﻿using Nwazet.Commerce.Models;
-
-namespace Nwazet.Commerce.Services {
-    public interface IGoogleCheckoutService : ICheckoutService {
-        GoogleCheckoutSettingsPart GetSettings();
-    }
-}
