ff718c9f20916da213021d42d1d676b1d41e595c
Josh Berry <josh@berryweblabs.com>
2014-03-02 11:56:41 -0600

    Missing files for US zip code tax provider
    --HG--
    branch : zipcode_tax

---
+++ Drivers/ZipCodeTaxPartDriver.cs
@@ -0,0 +1,46 @@
+﻿using Nwazet.Commerce.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Environment.Extensions;
+
+namespace Nwazet.Commerce.Drivers {
+    [OrchardFeature("Nwazet.Taxes")]
+    public class ZipCodeTaxPartDriver : ContentPartDriver<ZipCodeTaxPart> {
+
+        protected override string Prefix {
+            get { return "NwazetCommerceUnitesStatesZipCodeTax"; }
+        }
+
+        //GET
+        protected override DriverResult Editor(ZipCodeTaxPart part, dynamic shapeHelper) {
+            return ContentShape("Parts_ZipCodeTax_Edit",
+                () => shapeHelper.EditorTemplate(
+                    TemplateName: "Parts/ZipCodeTax",
+                    Model: shapeHelper.ZipCodeTax(
+                        Tax: part,
+                        Prefix: Prefix),
+                    Prefix: Prefix));
+        }
+
+        //POST
+        protected override DriverResult Editor(ZipCodeTaxPart part, IUpdateModel updater, dynamic shapeHelper) {
+            updater.TryUpdateModel(part, Prefix, null, null);
+            //part.Rate = part.Rate/100; // User will input a percentage
+            return Editor(part, shapeHelper);
+        }
+
+        protected override void Importing(ZipCodeTaxPart part, ImportContentContext context) {
+            var el = context.Data.Element(typeof(ZipCodeTaxPart).Name);
+            if (el == null) return;
+            el.With(part)
+                .FromAttr(p => p.Priority);
+        }
+
+        protected override void Exporting(ZipCodeTaxPart part, ExportContentContext context) {
+            context.Element(typeof(ZipCodeTaxPart).Name)
+                .With(part)
+                .ToAttr(p => p.Priority);
+        }
+    }
+}
