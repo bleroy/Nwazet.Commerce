ac701401cec791429b8225cb5c15f8676fd28af7
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-08-09 18:28:38 -0700

    Reports made DRYer by implementing a base class

--- Reports/NumberOfSalesReport.cs
+++ Reports/NumberOfSalesReport.cs
@@ -11,59 +11,36 @@ using Orchard.Localization;
 
 namespace Nwazet.Commerce.Reports {
     [OrchardFeature("Nwazet.Reports")]
-    public class NumberOfSalesReport : ICommerceReport {
-        private readonly IContentManager _contentManager;
-
-        public NumberOfSalesReport(IContentManager contentManager) {
-            _contentManager = contentManager;
-            T = NullLocalizer.Instance;
+    public class NumberOfSalesReport : BaseSalesReport {
+        public NumberOfSalesReport(IContentManager contentManager) : base(contentManager) {
         }
 
-        public Localizer T { get; set; }
-
-        public string Name {
+        public override string Name {
             get { return T("Number of orders").Text; }
         }
 
-        public string Description {
+        public override string Description {
             get { return T("Number of distinct successful orders").Text; }
         }
 
-        public string DescriptionColumnHeader { get { return T("Period").Text; } }
-        public string ValueColumnHeader { get { return T("Number of orders").Text; } }
-        public string ValueFormat { get { return null; } }
+        public override string DescriptionColumnHeader {
+            get { return T("Period").Text; }
+        }
 
-        public ChartType ChartType { get { return ChartType.Line; } }
+        public override string ValueColumnHeader {
+            get { return T("Number of orders").Text; }
+        }
 
-        public IEnumerable<ReportDataPoint> GetData(DateTime startDate, DateTime endDate, TimePeriod granularity) {
-            startDate = granularity.BeginningDate(startDate);
-            endDate = granularity.EndingDate(endDate);
-            var orders = _contentManager
-                .Query<CommonPart, CommonPartRecord>("Order")
-                .Where(r =>
-                    r.CreatedUtc >= startDate.ToUniversalTime()
-                    && r.CreatedUtc <= endDate.ToUniversalTime())
-                .OrderBy(r => r.CreatedUtc)
-                .Join<OrderPartRecord>()
-                .Where(order => order.Status != OrderPart.Cancelled)
-                .List()
-                .ToList();
-            var numberOfPoints = granularity.PeriodCount(startDate, endDate);
-            var results = new List<ReportDataPoint>(numberOfPoints);
-            var intervalStart = startDate;
-            var intervalEnd = startDate + granularity;
-            while (intervalStart < endDate) {
-                var count = orders.Count(
-                    order => order.CreatedUtc >= intervalStart
-                             && order.CreatedUtc < intervalEnd);
-                results.Add(new ReportDataPoint {
-                    Description = granularity.ToString(intervalStart, CultureInfo.CurrentUICulture),
-                    Value = count
-                });
-                intervalStart = intervalEnd;
-                intervalEnd = intervalStart + granularity;
-            }
-            return results;
+        public override string ValueFormat {
+            get { return null; }
+        }
+
+        public override ChartType ChartType {
+            get { return ChartType.Line; }
+        }
+
+        public override double ComputeResultForInterval(IList<OrderPart> ordersForInterval) {
+            return ordersForInterval.Count();
         }
     }
 }
--- Reports/TotalSalesReport.cs
+++ Reports/TotalSalesReport.cs
@@ -1,73 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Nwazet.Commerce.Models;
 using Nwazet.Commerce.Models.Reporting;
 using Orchard.ContentManagement;
-using Orchard.Core.Common.Models;
 using Orchard.Environment.Extensions;
-using Orchard.Localization;
 
 namespace Nwazet.Commerce.Reports {
     [OrchardFeature("Nwazet.Reports")]
-    public class TotalSalesReport : ICommerceReport {
-        private readonly IContentManager _contentManager;
-
-        public TotalSalesReport(IContentManager contentManager) {
-            _contentManager = contentManager;
-            T = NullLocalizer.Instance;
+    public class TotalSalesReport : BaseSalesReport {
+        public TotalSalesReport(IContentManager contentManager) : base(contentManager) {
         }
 
-        public Localizer T { get; set; }
-
-        public string Name {
+        public override string Name {
             get { return T("Sales").Text; }
         }
 
-        public string Description {
+        public override string Description {
             get { return T("Total sale amount").Text; }
         }
 
-        public string DescriptionColumnHeader { get { return T("Period").Text; } }
-        public string ValueColumnHeader { get { return T("Amount").Text; } }
-        public string ValueFormat { get { return "c"; } }
+        public override string DescriptionColumnHeader {
+            get { return T("Period").Text; }
+        }
 
-        public ChartType ChartType { get { return ChartType.Line; } }
+        public override string ValueColumnHeader {
+            get { return T("Amount").Text; }
+        }
 
-        public IEnumerable<ReportDataPoint> GetData(DateTime startDate, DateTime endDate, TimePeriod granularity) {
-            startDate = granularity.BeginningDate(startDate);
-            endDate = granularity.EndingDate(endDate);
-            var orders = _contentManager
-                .Query<CommonPart, CommonPartRecord>("Order")
-                .Where(r =>
-                    r.CreatedUtc >= startDate.ToUniversalTime()
-                    && r.CreatedUtc <= endDate.ToUniversalTime())
-                .OrderBy(r => r.CreatedUtc)
-                .Join<OrderPartRecord>()
-                .Where(order => order.Status != OrderPart.Cancelled)
-                .List()
-                .ToList();
-            var numberOfPoints = granularity.PeriodCount(startDate, endDate);
-            var results = new List<ReportDataPoint>(numberOfPoints);
-            var intervalStart = startDate;
-            var intervalEnd = startDate + granularity;
-            while (intervalStart < endDate) {
-                var ordersForInterval = orders.Where(
-                    order => order.CreatedUtc >= intervalStart
-                             && order.CreatedUtc < intervalEnd)
-                    .ToList();
-                var amount = ordersForInterval.Any()
-                    ? ordersForInterval.Sum(order => order.As<OrderPart>().AmountPaid)
-                    : 0.0;
-                results.Add(new ReportDataPoint {
-                    Description = granularity.ToString(intervalStart, CultureInfo.CurrentUICulture),
-                    Value = amount
-                });
-                intervalStart = intervalEnd;
-                intervalEnd = intervalStart + granularity;
-            }
-            return results;
+        public override string ValueFormat {
+            get { return "c"; }
+        }
+
+        public override ChartType ChartType {
+            get { return ChartType.Line; }
+        }
+
+        public override double ComputeResultForInterval(IList<OrderPart> ordersForInterval) {
+            return ordersForInterval.Any()
+                ? ordersForInterval.Sum(order => order.AmountPaid)
+                : 0.0;
         }
     }
 }
