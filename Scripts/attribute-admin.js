0f7eb0ec70667a80e96817fa10408cac92435dd9
Josh Berry <josh@berryweblabs.com>
2014-09-17 09:36:35 -0500

    Moved new option markup into a template, fixed proj file
    --HG--
    branch : attribute_drag_sorting

--- Scripts/attribute-admin.js
+++ Scripts/attribute-admin.js
@@ -1,15 +1,12 @@
-﻿$(function() {
+﻿$(function () {
+
     var i = $("#NwazetCommerceAttribute_AttributeValues > li.option").length; // indexer for mvc model binding new tiers
 
     $("#NwazetCommerceAttribute_AddAttributeValue").click(function (event) {
         event.preventDefault();
-        $("#NwazetCommerceAttribute_AttributeValues").append('<li class="option">' +
-                                                        '<div class="option-name"><input type="hidden" name="NwazetCommerceAttribute.AttributeValues.Index" value="' + i + '" />' +
-                                                        '<input type="hidden" name="NwazetCommerceAttribute.AttributeValues[' + i + '].SortOrder" value="' + (i + 1) + '" />' +
-                                                        '<input type="text" class="text" name="NwazetCommerceAttribute.AttributeValues[' + i + '].Text" value="" /></div>\r\n' +
-                                                        '<div class="price-adj"><input type="text" class="text small" name="NwazetCommerceAttribute.AttributeValues[' + i + '].PriceAdjustment" value="0" /></div>\r\n' +
-                                                        '<div class="apply-line"><input type="checkbox" name="NwazetCommerceAttribute.AttributeValues[' + i + '].IsLineAdjustment" value="True" /></div>\r\n' +
-                                                        '<div><a href="#" class="nwazet-remove-attribute-value">Delete</a></div></li>');
+        var valueTemplate = $("#valueTemplate").html();
+        $("#NwazetCommerceAttribute_AttributeValues").append(
+            Mustache.render(valueTemplate, { index: i, sort: i + 1 }));
         i++;
     });
 
@@ -19,8 +16,8 @@
     });
 
     $("#NwazetCommerceAttribute_AttributeValues").sortable({
-        update: function(event, ui) {
-            $.each($(this).children("li"), function() {
+        update: function (event, ui) {
+            $.each($(this).children("li"), function () {
                 var $row = $(this);
                 $row.find("input[name$=SortOrder]").val($row.index());
             });
