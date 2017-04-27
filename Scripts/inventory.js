f9b2e2c309d970066581a5f3aac240334fc3afee
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-04-27 17:23:41 +0200

    reverted the change to inventory.js, because it's not actually needed

--- Scripts/inventory.js
+++ Scripts/inventory.js
@@ -15,20 +15,4 @@
             });
         return false;
     });
-    $(".inventory .addone").click(function () {
-        $.post($(this).data("url"), {
-            id: $(this).data("id"),
-            __RequestVerificationToken: $("input[name=__RequestVerificationToken]").val()
-        }, function (data) {
-            for (var sku in data) {
-                $(".inventory .value." + sku)
-                    .data("inventory", data[sku])
-                    .slideUp(function () {
-                        var that = $(this);
-                        that.html(that.data("inventory")).slideDown();
-                    });
-            }
-        });
-        return false;
-    });
 });
