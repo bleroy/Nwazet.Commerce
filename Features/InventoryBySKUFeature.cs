aa73ae54778a8c726ad4c8045824811bb21c4b30
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-04-28 14:16:47 +0200

    Added notification banner to tell when the inventories are not synchronized Added IFeatureEventHandler to call the method to verify the inventories when the feature is enabled

---
+++ Features/InventoryBySKUFeature.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nwazet.Commerce.Models;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Environment;
+using Orchard.Environment.Extensions.Models;
+
+namespace Nwazet.Commerce.Features {
+    public class InventoryBySKUFeature : IFeatureEventHandler {
+        private readonly IOrchardServices _orchardServices;
+        public InventoryBySKUFeature(
+            IOrchardServices orchardServices) {
+
+            _orchardServices = orchardServices;
+        }
+
+        public void Enabled(Feature feature) {
+            if (feature.Descriptor.Id == "Nwazet.InventoryBySKU") {
+                //the feature has just been enabled, so maybe the inventories are not in synch
+                _orchardServices.WorkContext.CurrentSite.As<InventoryBySKUSiteSettingsPart>().InventoriesAreAllInSynch = false;
+                //check whether inventories are already synchronized
+            }
+        }
+
+        #region Not used interface methods
+        public void Disabled(Feature feature) {
+        }
+
+        public void Disabling(Feature feature) {
+        }
+
+        public void Enabling(Feature feature) {
+        }
+
+        public void Installed(Feature feature) {
+        }
+
+        public void Installing(Feature feature) {
+        }
+
+        public void Uninstalled(Feature feature) {
+        }
+
+        public void Uninstalling(Feature feature) {
+        }
+        #endregion
+    }
+}
--- Models/InventoryBySKUSiteSettingsPart.cs
+++ Models/InventoryBySKUSiteSettingsPart.cs
@@ -9,5 +9,15 @@ using Orchard.Environment.Extensions;
 namespace Nwazet.Commerce.Models {
     [OrchardFeature("Nwazet.InventoryBySKU")]
     public class InventoryBySKUSiteSettingsPart : ContentPart {
+        /// <summary>
+        /// Tells whether we have already verified the state of inventories. If this flag is true, it means we found that
+        /// inventories were synchronized across products. If this flag is false, we either haven't checked, or we found
+        /// that not all inventories were synchronized.
+        /// </summary>
+        public bool InventoriesAreAllInSynch
+        {
+            get { return this.Retrieve(p => p.InventoriesAreAllInSynch); }
+            set { this.Store(p => p.InventoriesAreAllInSynch, value); }
+        }
     }
 }
