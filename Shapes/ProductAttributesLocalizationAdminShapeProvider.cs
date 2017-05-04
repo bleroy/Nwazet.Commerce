d36e77647679ad647c3bb2f0e389e1e83ffb3dbb
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-05-04 12:45:11 +0200

    CHanged the base view of AttributesAdminController.Index to use shapes. implemented shapes and alternate for localization

---
+++ Shapes/ProductAttributesLocalizationAdminShapeProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard.DisplayManagement.Descriptors;
+using Orchard.Environment.Extensions;
+
+namespace Nwazet.Commerce.Shapes {
+    [OrchardFeature("Nwazet.AttributesLocalizationExtension")]
+    public class ProductAttributesLocalizationAdminShapeProvider : IShapeTableProvider {
+        public void Discover(ShapeTableBuilder builder) {
+            builder.Describe("ProductAttributesAdminListItem")
+                .OnDisplaying(displaying => {
+                    displaying.ShapeMetadata.Alternates.Add("ProductAttributesLocalizationAdminListItem");
+                });
+        }
+    }
+}
