ff7466b221b16851296397143a95bbff8267be58
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2017-06-13 17:54:07 +0200

    - Views for Creating WishList and Edit WishLists' SettingsShape - Removed modal window logic beacause it introduced too much front-end complexity

--- ViewModels/WishListsIndexViewModel.cs
+++ ViewModels/WishListsIndexViewModel.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace Nwazet.Commerce.ViewModels {
     public class WishListsIndexViewModel {
-        public dynamic CreateShape { get; set; }
-        public dynamic SettingsShape { get; set; }
+        //public dynamic CreateShape { get; set; }
+        //public dynamic SettingsShape { get; set; }
         public IEnumerable<WishListListPart> WishLists { get; set; }
         public WishListListPart WishList { get; set; }
         public dynamic WishListView { get; set; }
